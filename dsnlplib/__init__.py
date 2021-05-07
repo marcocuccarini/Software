380e83d9e0b013cc7fcba13454af28854cdb051f
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-05-07 13:15:53 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -396,7 +396,7 @@ class DSExperiment(object):
     learn = Learner(dls, fai_model,  opt_func=self.opt_func, loss_func=self.criterion, metrics=self.metrics, cbs=self.callbacks, splitter=fai_model.transformer_spltr).to_fp16()
 
    	if (pretrained):
-   		learn.load(fname)
+      learn.load(fname)
 
     print(training_id, flush=true)
     return training_id, learn
