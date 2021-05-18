3350e0baa4b43f1cee2e1434d2bb5c685d8932ac
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-05-18 07:38:01 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -566,12 +566,7 @@ class DSExperiment(object):
       for train_index, valid_index in dsc.split_series[start_from_split:]:
        training_id, learn = self.create_learner(train_index, valid_index)
         
-      	'''if(('modello.json').exists()):
-
-      	  json_file = open('modello.json', 'r')
-      	  loaded_model_json = json_file.read()
-      	  json_file.close()
-      	  learn = model_from_json(loaded_model_json)'''
+      	
 
       	if (dsc.epochs > 0):
 
