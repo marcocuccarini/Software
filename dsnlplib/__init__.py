7ac3c7e74c8827b8a219550cbc4ef050671b3a0f
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-05-20 18:33:15 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -244,7 +244,7 @@ class DSConfig(object):
       self.use_activ = kwargs.pop("use_activ", True) 
 
       # Indica sino a quale gruppo di layer congelare l'apprendimento
-      self.freeze_to = kwargs.pop("freeze_to",  1)
+      self.freeze_to = kwargs.pop("freeze_to",  10)
 
       # Numero di filtri per la cnn
       self.kernel_num = kwargs.pop("kernel_num",  3)
