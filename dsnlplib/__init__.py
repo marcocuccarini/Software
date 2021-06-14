514791159845fffc25f1db9829bb3d69e128714f
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-06-14 22:00:32 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -750,7 +750,7 @@ class DSClassificationInterpretation(ClassificationInterpretation):
         cm = confusion_matrix(y_t, y_p, cmap=plt.cm.Blues)
         cmn = cm.astype('float')/cm.sum(axis=1)[:, np.newaxis]
         fig, ax = plt.subplots(figsize=(14,14))
-        sns.heatmap(cmn, annot=True, fmt='.2f', xticklabels=voc, yticklabels=voc)
+        sns.heatmap(cmn, annot=True, fmt='.2f', xticklabels=voc, yticklabels=voc ,cmap=plt.cm.Blues)
         plt.ylabel('Actual')
         plt.xlabel('Predicted')
         plt.show(block=False)
