d8e4d7bb0e761fe3f4a6159136e308b00f65af90
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-06-14 11:40:26 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -830,7 +830,7 @@ class DSClassificationInterpretation(ClassificationInterpretation):
         else:
           cm = np.mean(confusion_matrices,axis=0)
           norm_dec = 0
-        print(most_confused(cm,self.vocab))
+        print(interp.most_confused(cm,self.vocab))
         if plot_txt:
             thresh = ncm.max() / 2.
             for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
