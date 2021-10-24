2ce810e707858b8e613ef56688a45d3aaaaa990a
marcocuccarini <59500902+marcocuccarini@users.noreply.github.com>
2021-10-24 22:54:03 +0200

    Update __init__.py

--- dsnlplib/__init__.py
+++ dsnlplib/__init__.py
@@ -772,7 +772,8 @@ class DSClassificationInterpretation(ClassificationInterpretation):
         df2 = pd.DataFrame(columns = ['d','t'])
         df2['d']=d
         df2['t']=t
-        df2.to_csv('/content/drive/MyDrive/BorsadiStudioNLP/FakevsReal-News/DatasetFakeNews/ResultTest.csv')
+
+        df2.to_csv('ResultTest.csv')
 
         df1 = pd.DataFrame(columns = ['Label','Metrica'])
         df1['Label']=voc
